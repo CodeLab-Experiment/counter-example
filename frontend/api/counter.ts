0fc4943f1464e0c34829532f6df4c8b4651a7e6f
Le Khac Thanh Tung <lekhacthanhtung.it@gmail.com>
2024-01-28 00:25:30 +0700

    feat: deploy & init contract

--- frontend/api/counter.ts
+++ frontend/api/counter.ts
@@ -2,9 +2,12 @@ import useSWR from "swr";
 import { getAddress, getClient, getSigningClient } from "../lib/client";
 import { getContractAddr } from "../lib/state";
 
+const sender = "orai1yzsegvns6vmvf5q29uv26p3th4fd2kzmsq3h6m";
+const funds = [];
+
 export const getCount = async () => {
   const client = await getClient();
-  return await client.queryContractSmart(getContractAddr(), { get_count: {} });
+  return await client.queryContractSmart(getContractAddr(), { token_info: {} });
 };
 
 export const increase = async () => {
@@ -12,16 +15,46 @@ export const increase = async () => {
   return await client.execute(
     await getAddress(),
     getContractAddr(),
-    { increment: {} },
+    {
+      transfer: {
+        recipient: "orai1mun6zs5m9tp2e54m8nvvhvc2xg2dg7u5vd2jp3",
+        amount: "1000000",
+      },
+    },
     "auto"
   );
 };
 
 export const useCount = () => {
   const { data, error, mutate } = useSWR("/counter/count", getCount);
+  console.log(data);
   return {
-    count: data?.count,
+    count: data?.decimals,
     error,
     increase: () => mutate(increase),
   };
+};
+
+export const deployContract = async () => {
+  const client = await getSigningClient();
+  const arrayBuffer = await fetch(
+    "https://node1-gateway-ipfs.eueno.io/ipfs/QmbUc8xHLPy8WUTA6dtnmkzGKLTBeKtV1Kvir9JNraX6gp"
+  ).then((res) => res.arrayBuffer());
+  const wasmCode = new Uint8Array(arrayBuffer);
+  const { codeId } = await client.upload(sender, wasmCode, "auto");
+  console.log(codeId)
+
+  const { contractAddress } = await client.instantiate(
+    sender,
+    codeId,
+    {
+      name: "NFT contract",
+      symbol: "NFT",
+      minter: "orai1yzsegvns6vmvf5q29uv26p3th4fd2kzmsq3h6m",
+    },
+    "Counter",
+    "auto"
+  );
+  console.log(contractAddress);
+  console.log(codeId);
 };
--- frontend/lib/state.ts
+++ frontend/lib/state.ts
@@ -1,19 +1,5 @@
-import state from "../.beaker/state.json";
-import stateLocal from "../.beaker/state.local.json";
-
-const getState = () => {
-  if (!process.env.NEXT_PUBLIC_NETWORK) {
-    throw Error("`NEXT_PUBLIC_NETWORK` env variable not found, please set");
-  }
-  return {
-    ...state,
-    ...stateLocal,
-  }[process.env.NEXT_PUBLIC_NETWORK];
-};
-
 export const getContractAddr = () => {
-  //const contractAddr = getState()?.counter.addresses.default;
-  const contractAddr = "orai17mq0sg6ey9u2c5pwjjn8l6sppacrrzfucx5r97pcn25d6mjuax2qza6jar";
+  const contractAddr = "orai16c65vpgyvs46yvde37yn6cp7rqplccz6uzg8f47lkvu89gqw49uqdv44t0";
 
   if (!contractAddr) {
     throw Error("Contract address not found, please check your state file");
